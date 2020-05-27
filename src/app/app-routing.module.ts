e685157b13af2b09adcfacf8453a8a61c047857d
Valter07 <zuliani13@hotmail.com>
2020-05-27 12:11:55 -0500

    partida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { TemaListComponent } from './pages/tema-list/tema-list.component';
 import { TemaComponent } from './pages/tema/tema.component';
 import { TemaResultComponent } from './pages/tema-result/tema-result.component';
 import { RespuestaListComponent } from './pages/respuesta-list/respuesta-list.component';
+import { PartidaComponent } from './pages/partida/partida.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '', component: TemaListComponent },
+  { path: '', component: PartidaComponent },
+  { path: 'temalist', component: TemaListComponent },
   { path: 'tema/:id', component: TemaComponent },
   { path: 'tema/:id/results', component: TemaResultComponent },
   { path: 'tema/:id/respuestas', component: RespuestaListComponent }
